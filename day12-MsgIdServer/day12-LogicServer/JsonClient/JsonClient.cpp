e48723596143bf243357c8269ac1425cf64a83aa
EnzoFerrari430 <782794844@qq.com>
2025-03-31 00:28:47 +0800

    添加工程，修改msgnode协议，为后续逻辑层架构做准备

---
+++ day12-MsgIdServer/day12-LogicServer/JsonClient/JsonClient.cpp
@@ -0,0 +1,86 @@
+﻿#include <iostream>
+#include <boost/asio.hpp>
+#include <thread>
+
+#include <json/json.h>
+#include <json/value.h>
+#include <json/reader.h>
+
+using namespace std;
+using namespace boost::asio::ip;
+const int MAX_LENGTH = 1024 * 2;
+const int HEAD_LENGTH = 2;
+const int HEAD_TOTAL_LEN = 4;
+int main()
+{
+    try {
+        //创建上下文服务
+        boost::asio::io_context   ioc;
+        //构造endpoint
+        tcp::endpoint  remote_ep(make_address("127.0.0.1"), 10086);
+        tcp::socket  sock(ioc);
+        boost::system::error_code   error = boost::asio::error::host_not_found; ;
+        sock.connect(remote_ep, error);
+        if (error) {
+            cout << "connect failed, code is " << error.value() << " error msg is " << error.message();
+            return 0;
+        }
+
+        Json::Value root;
+        root["id"] = 1001;
+        root["data"] = "hello world";
+        std::string request = root.toStyledString();
+        short request_length = request.length();
+        char send_data[MAX_LENGTH] = { 0 };
+
+        // 消息id
+        short msgid = 1001;
+        msgid = boost::asio::detail::socket_ops::host_to_network_short(msgid);
+        memcpy(send_data, &msgid, 2);
+
+        // 转为网络字节序
+        short request_host_length = boost::asio::detail::socket_ops::host_to_network_short(request_length);
+        memcpy(send_data + 2, &request_host_length, 2);
+        memcpy(send_data + HEAD_TOTAL_LEN, request.c_str(), request_length);
+        boost::asio::write(sock, boost::asio::buffer(send_data, request_length + HEAD_TOTAL_LEN));
+
+
+
+        cout << "begin to receive..." << endl;
+        char reply_head[HEAD_TOTAL_LEN];
+        size_t reply_length = boost::asio::read(sock, boost::asio::buffer(reply_head, HEAD_TOTAL_LEN));
+        msgid = 0;
+        memcpy(&msgid, reply_head, HEAD_LENGTH);
+        // 将网络字节序转换成本地字节序
+        msgid = boost::asio::detail::socket_ops::network_to_host_short(msgid);
+
+        short msglen = 0;
+        memcpy(&msglen, reply_head + HEAD_LENGTH, HEAD_LENGTH);
+        // 将网络字节序转换成本地字节序
+        msglen = boost::asio::detail::socket_ops::network_to_host_short(msglen);
+        char msg[MAX_LENGTH] = { 0 };
+        size_t  msg_length = boost::asio::read(sock, boost::asio::buffer(msg, msglen));
+
+        // 反序列化
+        Json::Value root2;
+        Json::Reader reader;
+        reader.parse(msg, msg + msg_length, root2);
+        std::cout << "msg id is " << root2["id"] << " msg is " << root2["data"] << std::endl;
+        getchar();
+    }
+    catch (std::exception& e) {
+        std::cerr << "Exception: " << e.what() << endl;
+    }
+    return 0;
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
---
+++ day12-MsgIdServer/day12-LogicServer/day12-LogicServer/CSession.h
@@ -0,0 +1,72 @@
+﻿#pragma once
+
+#include <iostream>
+#include <queue>
+#include <boost/asio.hpp>
+#include <boost/uuid/uuid_generators.hpp>
+#include <boost/uuid/uuid_io.hpp>
+#include "const.h"
+#include "MsgNode.h"
+using boost::asio::ip::tcp;
+
+class CServer;
+
+/*
+std::enable_shared_from_this 是 C++ 标准库中的一个模板类，它在 <memory> 头文件中定义。
+其主要作用是让一个对象能够安全地从其内部获取一个指向自身的 std::shared_ptr。
+
+在使用 std::shared_ptr 管理对象生命周期时，有时对象自身需要创建一个指向自己的 std::shared_ptr.
+但直接使用 this 指针创建 std::shared_ptr 会引发问题，
+因为这样会产生多个独立管理同一对象的 std::shared_ptr，从而可能导致对象被多次删除，产生未定义行为。
+
+要使用 std::enable_shared_from_this，需要让类继承自 std::enable_shared_from_this<类名>，
+然后在类的成员函数中可以调用 shared_from_this() 方法来获取一个指向当前对象的 std::shared_ptr。
+*/
+
+
+class CSession : public std::enable_shared_from_this<CSession>
+{
+public:
+    CSession(boost::asio::io_context& ioc, CServer* server);
+
+    ~CSession();
+
+    tcp::socket& GetSocket();
+    std::string& GetUuid();
+    void Start();
+    void Send(char* msg, int max_length, short msgid);
+    void Send(std::string msg, short msgid);
+    void Close();
+    std::shared_ptr<CSession> SharedSelf();
+private:
+    void PrintRecvData(char* data, int length);
+    // echo类型的服务器
+    // 这里的回调增加一个智能指针参数 延长了内存的声明周期
+    void HandleRead(const boost::system::error_code& error, std::size_t bytes_transferred, std::shared_ptr<CSession> _self_shared);
+    void HandleWrite(const boost::system::error_code& error, std::shared_ptr<CSession> _self_shared);
+
+private:
+    tcp::socket _socket;
+    char _data[MAX_LENGTH];
+    CServer* _server;
+    std::string _uuid;
+    bool _b_close;
+
+    /*
+    因为全双工要有自己的发送接口，然而我们不能保证每次调用的发送接口的时候上一次的数据已经发送完，
+    所以我们要把要发送的数据放入到队列中，通过回调不停的发送。
+    而且我们不能保证发送的接口和回调的函数的接口在一个线程里，所以要增加一个锁保证队列的安全性 
+    */
+    std::queue<std::shared_ptr<SendNode>> _send_que;
+    std::mutex _send_lock;
+
+    /*
+    _recv_msg_node用来存储接受的消息体信息
+    _recv_head_node用来存储接收的头部信息
+    _b_head_parse表示是否处理完头部信息
+    */
+    std::shared_ptr<RecvNode> _recv_msg_node;
+    bool _b_head_parse;
+    std::shared_ptr<MsgNode> _recv_head_node;
+};
+
